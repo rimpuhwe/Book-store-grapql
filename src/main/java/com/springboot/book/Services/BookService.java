d4b6b058f3efa7d95cc65947435d97cdd4531a52
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-10-17 11:44:05 +0200

    add the business logic

--- src/main/java/com/springboot/book/Services/BookService.java
+++ src/main/java/com/springboot/book/Services/BookService.java
@@ -1,4 +1,38 @@
 package com.springboot.book.Services;
 
+import com.springboot.book.Entities.BookEntity;
+import com.springboot.book.Records.Book;
+import com.springboot.book.Records.CreateBookInput;
+import com.springboot.book.Repositories.BookRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookService {
+    private BookRepository repository;
+
+    public Book createBook(CreateBookInput book) {
+        BookEntity bookEntity = new BookEntity();
+        bookEntity.setBookName(book.name());
+        bookEntity.setAuthor(book.author());
+        bookEntity.setPageCount(book.pageCount());
+        repository.save(bookEntity);
+        return new Book(bookEntity.getBookName(), bookEntity.getAuthor(), bookEntity.getPageCount());
+    }
+    public List<Book> getAllBooks() {
+        return  repository.findAll()
+                           .stream()
+                           .map(entity -> new Book(entity.getBookName(), entity.getAuthor(), entity.getPageCount()))
+                           .collect(Collectors.toList());
+    }
+
+
+
+
 }
