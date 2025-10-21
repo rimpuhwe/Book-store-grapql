e351a1b15ca0625d0da8dc0985d3a38efb5afc8f
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-10-21 08:00:43 +0200

    added the business for the mutations

--- src/main/java/com/springboot/book/Services/BookService.java
+++ src/main/java/com/springboot/book/Services/BookService.java
@@ -3,9 +3,9 @@ package com.springboot.book.Services;
 import com.springboot.book.Entities.BookEntity;
 import com.springboot.book.Records.Book;
 import com.springboot.book.Records.CreateBookInput;
+import com.springboot.book.Records.UpdateBookInput;
 import com.springboot.book.Repositories.BookRepository;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,9 +13,9 @@ import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
-@NoArgsConstructor
+
 public class BookService {
-    private BookRepository repository;
+    private final BookRepository repository;
 
     public Book createBook(CreateBookInput book) {
         BookEntity bookEntity = new BookEntity();
@@ -30,6 +30,32 @@ public class BookService {
                            .stream()
                            .map(entity -> new Book(entity.getBookName(), entity.getAuthor(), entity.getPageCount()))
                            .collect(Collectors.toList());
+    }
+    public Book updateBook(UpdateBookInput book) {
+      BookEntity  book1 = repository.findBookEntityById(book.id());
+      if (book1 != null) {
+          book1.setBookName(book.name());
+          book1.setAuthor(book.author());
+          book1.setPageCount(book.pageCount());
+      }
+      else{
+          throw new RuntimeException("Book not found");
+      }
+      repository.save(book1);
+      return new Book(book1.getBookName(), book1.getAuthor(), book1.getPageCount());
+
+    }
+    public boolean deleteBook(String name) {
+
+        BookEntity deleted = repository.findBookEntityByBookName(name);
+        if (deleted != null) {
+            repository.delete(deleted);
+        }
+        else {
+            throw new IllegalArgumentException("Book with name " + name+ " not found");
+        }
+        return true;
+
     }
 
 
