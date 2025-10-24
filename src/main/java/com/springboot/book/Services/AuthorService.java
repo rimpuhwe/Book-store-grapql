334d3bc5d34ec171b74fae7e6cfc42a1163640ca
rimpuhwe <rimpuhwemanzi@gmail.com>
2025-10-24 17:02:06 +0200

    added the service for the  the author

--- src/main/java/com/springboot/book/Services/AuthorService.java
+++ src/main/java/com/springboot/book/Services/AuthorService.java
@@ -1,4 +1,45 @@
 package com.springboot.book.Services;
 
+import com.springboot.book.Entities.AuthorEntity;
+import com.springboot.book.Records.Author;
+import com.springboot.book.Records.AuthorInput;
+import com.springboot.book.Records.Book;
+import com.springboot.book.Repositories.AuthorRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
 public class AuthorService {
+    private final AuthorRepository authorRepository;
+
+    public Author createNewAuthor(AuthorInput author) {
+        AuthorEntity newAuthor =  new AuthorEntity();
+        newAuthor.setName(author.name());
+        newAuthor.setEmail(author.email());
+
+        authorRepository.save(newAuthor);
+
+        return  new Author(newAuthor.getName(), newAuthor.getEmail(), Collections.emptyList() );
+
+    }
+    public List<Author> getAllAuthors(){
+        return  authorRepository.findAll().stream()
+                                          .map(
+                                                  author -> new Author(
+                                                          author.getName(),
+                                                          author.getEmail(),
+                                                          author.getBooks().stream()
+                                                                           .map(book -> new Book(
+                                                                                   book.getBookName(),
+                                                                                   null,
+                                                                                   book.getPageCount()
+                                                                                   )
+                                                                           ).collect(Collectors.toList())))
+                                          .collect(Collectors.toList());
+    }
 }
--- src/main/java/com/springboot/book/Services/BookService.java
+++ src/main/java/com/springboot/book/Services/BookService.java
@@ -1,4 +1,48 @@
 package com.springboot.book.Services;
 
+
+import com.springboot.book.Entities.AuthorEntity;
+import com.springboot.book.Entities.BookEntity;
+import com.springboot.book.Records.Author;
+import com.springboot.book.Records.AuthorInput;
+import com.springboot.book.Records.Book;
+import com.springboot.book.Records.CreateBookInput;
+import com.springboot.book.Repositories.AuthorRepository;
+import com.springboot.book.Repositories.BookRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
 public class BookService {
+
+    private BookRepository bookRepository;
+    private AuthorRepository authorRepository;
+
+    public Book createBook(int id,CreateBookInput input) {
+        AuthorEntity author  = authorRepository.findById(id).orElseThrow(() -> new RuntimeException("Author not found"));
+        BookEntity book = new BookEntity();
+        if (author != null) {
+            book.setAuthor(author);
+            book.setBookName(input.name());
+            book.setPageCount(input.pageCount());
+            bookRepository.save(book);
+        }
+
+        assert author != null;
+        return new Book(book.getBookName(),new Author(author.getName(),author.getEmail(),null), book.getPageCount());
+
+    }
+
+    public List<Book> getAllBook() {
+        return  bookRepository.findAll().stream()
+                                         .map(book -> new Book(book.getBookName(),new Author(book.getAuthor().getName(),book.getAuthor().getEmail(), Collections.emptyList()) , book.getPageCount()))
+                                         .collect(Collectors.toList());
+    }
+
+
 }
